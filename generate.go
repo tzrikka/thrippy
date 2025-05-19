e7cc9542ffc212a847cc99337c246e597b950693
Daniel Abraham <aeroquix@gmail.com>
2025-05-19 12:30:52 -0700

    Finish renaming

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
-package trippy
+package thrippy
 
 //go:generate protoc --proto_path=proto --proto_path=third_party --go_out=proto --go-grpc_out=proto --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative thrippy/v1/oauth.proto
 //go:generate protoc --proto_path=proto --proto_path=third_party --go_out=proto --go-grpc_out=proto --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative thrippy/v1/thrippy.proto
